0658384768362d38f8b06b7c868c7229d2e30fe8
2019051604044liChengYang <1945729758@qq.com>
2023-10-17 16:32:40 +0800

    basicdrawing 实例

--- main.cpp
+++ main.cpp
@@ -1,17 +1,27 @@
-#include <QGuiApplication>
-#include <QQmlApplicationEngine>
+/*
+ * 基础的绘画例子
+ *
+    Copyright (C) 2016 The Qt Company Ltd.
+    SPDX-License-Identifier: LicenseRef-Qt-Commercial OR BSD-3-Clause
 
+    The Basic Drawing example consists of two classes:
+
+    1.RenderArea is a custom widget that renders multiple copies of the currently active shape.
+    2.Window is the application's main window displaying a RenderArea widget in addition to several parameter widgets.
+*/
+
+
+#include "window.h"
+
+#include <QApplication>
 
 int main(int argc, char *argv[])
 {
-    QGuiApplication app(argc, argv);
+    // nitializes the resources specified by the .qrc file with the specified base name.
+    Q_INIT_RESOURCE(basicdrawing);
 
-    QQmlApplicationEngine engine;
-    const QUrl url(u"qrc:/qt/Main.qml"_qs);
-    QObject::connect(&engine, &QQmlApplicationEngine::objectCreationFailed,
-        &app, []() { QCoreApplication::exit(-1); },
-        Qt::QueuedConnection);
-    engine.load(url);
-
+    QApplication app(argc, argv);
+    Window window;
+    window.show();
     return app.exec();
 }
