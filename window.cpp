bde47b2c9fa70c92920c80698afc333273688637
2019051604044liChengYang <1945729758@qq.com>
2023-10-19 22:41:10 +0800

    QPixmap：将图片绘制到指定位置并使用画刷或者文字编辑图片

--- window.cpp
+++ window.cpp
@@ -54,11 +54,14 @@ void Window::paintEvent(QPaintEvent *event)
 
 
 
+/*1
     // 1.QImage，加载显示一个图片文件，并生成倒影图片文件并保存图片到指定路径
     QPainter painter(this);
     QImage image;
+
     //480 * 270
     image.load("../qt/images/leetCode.png");
+
     //qDebug() << image.size() <<image.format() << image.depth();
 
     painter.drawImage(QPoint(10, 10), image);
@@ -73,8 +76,49 @@ void Window::paintEvent(QPaintEvent *event)
     // 保存新文件
     newImage.save("../mirror.png");
 
+1*/
 
 
+
+
+
+
+    // 2.QPixmap：将图片绘制到指定位置并使用画刷或者文字编辑图片
+
+    /*
+     * QPixmap可以作为一个绘图设备将图像显示在屏幕上。QPixmap中的像素在内部由底层的窗口系统进行管理。
+     * 因为QPixmap是QPaintDevice的子类，所以QPainter也可以直接在它上面进行绘制。
+     *
+     * 要想访问像素，只能使用QPainter提供的相应函数，或者将QPixmap转换为QImage。
+     * 而与QImage不同的是，QPixmap中的fill()函数可以使用指定的颜色初始化整个pixmap图像。
+     *
+     * 可以使用toImage()和fromImage()函数在QImage和QPixmap之间进行转换。
+     * 通常情况下，QImage类用来加载一个图像文件，随意操纵图像数据，然后将QImage对象转换为QPixmap类型以将结果显示在屏幕上。当然，如果不需要对图像进行操作，那么也可以直接使用QPixmap来加载图像文件。
+     *
+     * 另外，与QImage不同的是，QPixmap依赖于具体的硬件。
+     *
+     * QPixmap可以很容易地通过QLabel或QAbstractButton的子类(如QPushButton)显示在屏幕上。
+     * QLabel拥有一个pixmap属性，而QAbstractButton拥有一个icon属性。
+     * QPixmap可以使用copy()复制图像上的一个区域，还可以使用mask()实现遮罩效果
+    */
+
+
+
+    QPainter painter(this);
+    QPixmap pix;
+    pix.load("../qt/images/leetCode.png");
+
+    // 绘制到指定位置
+    painter.drawPixmap(100, 0, pix.width(), pix.height(), pix);
+
+    // 对图片进行编辑绘画
+    // 画刷
+    painter.setBrush(QColor(0, 0, 255, 100));
+    painter.drawRect(100, 0, pix.width(), pix.height());
+
+    // 编辑文字
+    QRect rect(100, 0, 150, 150);
+    painter.drawText(rect, Qt::AlignCenter, tr("文字编辑"));
 
 
 
