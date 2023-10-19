73623ba3ddec72304b39711ee7090966a1deb447
2019051604044liChengYang <1945729758@qq.com>
2023-10-19 21:52:05 +0800

    QPainterPath 绘制一个复杂的图形并复用

--- window.cpp
+++ window.cpp
@@ -28,41 +28,32 @@ Window::Window()
 // 绘画
 void Window::paintEvent(QPaintEvent *event)
 {
-    // 绘制文字
+    // 绘制路径(多个绘制元素的集合，可以复用)
+
     /*
-     * 除了绘制图形以外，还可以使用QPainter::drawText()函数来绘制文字
-     * 也可以使用QPainter::setFont来设置绘制文字所使用的字体
-     * 使用QPainter::fontInfo()函数可以获取字体信息，它返回QFontInfo类对象。
-     * 绘制文字时会默认使用抗锯齿
+     * 如果要绘制一个复杂的图形，可以使用QPainterPath类，
+     * 并使用QPainter::drawPath()进行绘制。
+     * QPainterPath类为绘制操作提供了一个容器，可以用来创建图形并且重复使用。
+     *
+     * 一个绘图路径就是由多个矩形，椭圆，线条或者曲线等组成的对象。
+     * 一个路径可以是封闭的，如果矩形和椭圆；也可以是非封闭的，如线条和曲线。
     */
 
     QPainter painter(this);
-    QRect rect(10.0, 10.0, 400, 400);
-    painter.drawRect(rect);
+    QPainterPath path;
+    path.moveTo(50, 250);
+    path.lineTo(50, 230);       // 类似的函数还有arcTo(), cubicTo(), quadTo()
+    path.cubicTo(QPointF(105, 40), QPointF(115, 80), QPointF(120, 60)); // 绘制三次贝塞尔曲线(使用quadTo()绘制二次贝塞尔曲线)
+    path.lineTo(130, 130);
+    path.addEllipse(QPoint(130,130), 30, 30);       // 向路径中添加一个圆,与之类似的函数还有addPath(),addRect(),addRegion(),addText(),addPolygon()
 
-    painter.setPen(Qt::red);
+    painter.setPen(Qt::darkYellow);
+    painter.drawPath(path);		// 路径构建完成后使用drawPath()将路径绘制出来
 
-    //  绘制文字所在的矩形盒子，文字在盒子中的对齐方式，要绘制的文字内容，第四个参数一般可省
-    // 文字的多个不同的对齐方式可以使用’|'符号连接它们以同时使用
-    painter.drawText(rect, Qt::AlignCenter, tr("中心"));
-    painter.drawText(rect, Qt::AlignHCenter, tr("垂直居中"));
-    painter.drawText(rect, Qt::AlignRight, tr("右上"));
-
-
-    // 字体族，字体大小(默认12)，字体weight，是否使用斜体
-    QFont font("宋体", 15, QFont::Bold, true);
-    font.setUnderline(true);	// 下划线
-    font.setOverline(true);		// 中划线
-    font.setCapitalization(QFont::SmallCaps);	// 字母大写
-    font.setLetterSpacing(QFont::AbsoluteSpacing, 10);      // 设置字符间间距
-    painter.setFont(font);
-    painter.setPen(Qt::green);
-    painter.drawText(120, 80, tr("drawText()的另一种重载形式"));
-
-    // 平移和旋转坐标系
-    painter.translate(100, 100);
-    painter.rotate(90);
-    painter.drawText(0, 0, tr("heloQt"));
+    // 平移坐标系统
+    path.translate(200,0);
+    painter.setPen(Qt::darkBlue);
+    painter.drawPath(path);
 
 
 }
